09ef0f484868ad88d27e13870b13ac664f2c296c
Himanshu Goyal <hggoyal06@gmail.com>
2017-06-15 20:09:34 +0530

    Add files via upload

---
+++ django_cprofile_middleware/decorator.py
@@ -0,0 +1,11 @@
+from functools import wraps
+
+def my_decorator(extra_value):
+	def _my_decorator(view_func):
+		def _decorator(request, *args, **kwargs):
+			for n in range(1,extra_value,+1):
+				view_func(request, *args, **kwargs) 
+			response = view_func(request, *args, **kwargs)
+			return response
+		return wraps(view_func)(_decorator)
+	return _my_decorator
