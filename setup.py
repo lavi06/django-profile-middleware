fd3d3384efc33e330859da7e070417612c3d15d9
lavi06 <hggoyal06@gmail.com>
2017-06-15 18:34:24 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'python-cprofile-middleware',
+    packages = ['python-cprofile-middleware'],
+    license = 'MIT',
+    description = 'Easily add cProfile profiling to django views.',
+    author = 'Himanshu Goyal',
+    author_email = 'hggoyal06@gmail.com',
+    url = 'https://github.com/lavi06/python-cprofile-middleware',
+    download_url = 'https://github.com/lavi06/python-cprofile-middleware/download',
+    keywords = ['django','profiling','cProfile'],
+    classifiers = [],
+)
